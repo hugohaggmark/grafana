65b0e023fa2faf0225452944d8bea0bc4f57b698
Darren Janeczek <darren.janeczek@grafana.com>
2023-12-19 12:45:04 -0500

    fix: include breakdown query test

--- public/app/features/trails/AutomaticMetricQueries/AutoQueryEngine.test.ts
+++ public/app/features/trails/AutomaticMetricQueries/AutoQueryEngine.test.ts
@@ -68,7 +68,7 @@ describe('AutoQueryEngine', () => {
         'histogram_quantile(0.50, sum by(le) (rate(${metric}{${filters}}[$__rate_interval])))',
         's',
       ],
-    ])('Given metric named %s should generate query', (metric, expectedQuery, expectedUnit) => {
+    ])('Given metric named %s should generate preview query', (metric, expectedQuery, expectedUnit) => {
       const defs = getAutoQueriesForMetric(metric);
 
       if (typeof expectedQuery === 'string') {
@@ -77,6 +77,43 @@ describe('AutoQueryEngine', () => {
       } else {
         // For multiple queries
         const exprs = defs.preview.queries.map((query) => query.expr);
+        expect(exprs).toStrictEqual(expectedQuery);
+      }
+      expect(defs.main.unit).toEqual(expectedUnit);
+    });
+  });
+
+  describe('Breakdown query', () => {
+    it.each([
+      // General
+      ['my_metric_count', 'sum(rate(${metric}{${filters}}[$__rate_interval])) by(${groupby})', 'short'],
+      ['my_metric_seconds_count', 'sum(rate(${metric}{${filters}}[$__rate_interval])) by(${groupby})', 's'],
+      ['my_metric_bytes', 'avg(${metric}{${filters}}) by(${groupby})', 'bytes'],
+      ['my_metric_seconds', 'avg(${metric}{${filters}}) by(${groupby})', 's'],
+      ['my_metric_general', 'avg(${metric}{${filters}}) by(${groupby})', 'short'],
+      ['my_metric_seconds_total', 'sum(rate(${metric}{${filters}}[$__rate_interval])) by(${groupby})', 'short'],
+      // Sum
+      ['my_metric_seconds_sum', 'avg(rate(${metric}{${filters}}[$__rate_interval])) by(${groupby})', 's'],
+      // Bucket
+      [
+        'my_metric_bucket',
+        'histogram_quantile(0.50, sum by(le, ${groupby}) (rate(${metric}{${filters}}[$__rate_interval])))',
+        'short',
+      ],
+      [
+        'my_metric_seconds_bucket',
+        'histogram_quantile(0.50, sum by(le, ${groupby}) (rate(${metric}{${filters}}[$__rate_interval])))',
+        's',
+      ],
+    ])('Given metric named %s should generate breakdown query', (metric, expectedQuery, expectedUnit) => {
+      const defs = getAutoQueriesForMetric(metric);
+
+      if (typeof expectedQuery === 'string') {
+        // For single query
+        expect(defs.breakdown.queries[0].expr).toEqual(expectedQuery);
+      } else {
+        // For multiple queries
+        const exprs = defs.breakdown.queries.map((query) => query.expr);
         expect(exprs).toStrictEqual(expectedQuery);
       }
       expect(defs.main.unit).toEqual(expectedUnit);
